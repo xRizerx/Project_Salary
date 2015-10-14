491936f9bf6df2ab87713bbc171f152ffa748670
Cheezzie O_O <zplaygiirlz1@hotmail.com>
2015-10-14 11:32:01 +0700

    asdasd

---
+++ WEB_PERSONAL/WEB_PERSONAL/Connection/ConnectionDB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Rmutto.Connection
+{
+    public static class ConnectionDB
+    {
+        private static SqlConnection conn;
+        static ConnectionDB()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+            string connectionString = ConfigurationManager.ConnectionStrings["Connectionstring"].ToString();
+            conn = new SqlConnection(connectionString);
+        }
+
+        public static SqlConnection GetSqlConnection()
+        {
+            return conn;
+        }
+
+    }
+}
