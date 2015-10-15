48f00eb893d54ad67f710645598e9c31f1ecd6ab
Jomjak Tangnopakoon <tangkwa_ba_mark@hotmail.com>
2015-10-15 20:34:05 +0700

    mp footer

--- WEB_PERSONAL/WEB_PERSONAL/MasterPage.Master.designer.cs
+++ WEB_PERSONAL/WEB_PERSONAL/MasterPage.Master.designer.cs
@@ -220,15 +220,6 @@ namespace WEB_PERSONAL {
         protected global::System.Web.UI.WebControls.Label Label5;
         
         /// <summary>
-        /// Label4 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label4;
-        
-        /// <summary>
         /// Label6 control.
         /// </summary>
         /// <remarks>
