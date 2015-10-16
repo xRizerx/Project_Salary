3ff34a363ea3ceaa9d621bf353e105875205e6e8
Jomjak Tangnopakoon <tangkwa_ba_mark@hotmail.com>
2015-10-16 17:37:45 +0700

    mp login top

--- WEB_PERSONAL/WEB_PERSONAL/MasterPage.Master.designer.cs
+++ WEB_PERSONAL/WEB_PERSONAL/MasterPage.Master.designer.cs
@@ -76,6 +76,15 @@ namespace WEB_PERSONAL {
         protected global::System.Web.UI.WebControls.LinkButton LinkButton10;
         
         /// <summary>
+        /// Panel4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel4;
+        
+        /// <summary>
         /// Label2 control.
         /// </summary>
         /// <remarks>
