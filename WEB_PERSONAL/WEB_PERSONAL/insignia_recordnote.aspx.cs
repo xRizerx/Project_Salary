0803e127411c50b03e1d90e65ecb44f385316ca5
Tanakorn Na Ranong <tanakorncs8@gmail.com>
2015-10-19 16:18:48 +0700

    5555

---
+++ WEB_PERSONAL/WEB_PERSONAL/insignia_recordnote.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WEB_PERSONAL
+{
+    public partial class insignis_recordnote : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (RadioButton1.Checked == true)
+            { 
+                TextBox1.Text = "kuy1";
+            TextBox2.Text = "";
+            }
+            else if (RadioButton2.Checked == true)
+            { 
+                TextBox2.Text = "kuy2";
+            TextBox1.Text = "";
+            }
+
+        }
+    }
+}
