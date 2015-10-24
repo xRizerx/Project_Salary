91f496607a1e91298ca0cbb68abf30f5b3f7e613
Jomjak Tangnopakoon <tangkwa_ba_mark@hotmail.com>
2015-10-24 18:13:47 +0700

    mp study leave 1

--- WEB_PERSONAL/WEB_PERSONAL/Study-IN.aspx.designer.cs
+++ WEB_PERSONAL/WEB_PERSONAL/Study-IN.aspx.designer.cs
@@ -31,15 +31,6 @@ namespace WEB_PERSONAL {
         protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
-        /// Panel1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel Panel1;
-        
-        /// <summary>
         /// Label8 control.
         /// </summary>
         /// <remarks>
