e18c00eeac5f5bc067972e9c4b91aca146ae3ae9
xRizerx <praakaasit@hotmail.com>
2015-11-10 09:32:36 +0700

    Remove Excel

--- WEB_PERSONAL/WEB_PERSONAL/SalaryByID-Report.aspx.cs
+++ WEB_PERSONAL/WEB_PERSONAL/SalaryByID-Report.aspx.cs
@@ -2,7 +2,6 @@
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.IO;
-using Microsoft.Office.Interop.Excel;
 namespace WEB_PERSONAL
 {
     public partial class SalaryByID_Report : System.Web.UI.Page
@@ -15,54 +14,7 @@ namespace WEB_PERSONAL
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            Response.Clear();
-            Response.Buffer = true;
-            Response.AddHeader("content-disposition", "attachment;filename=SalaryByID-Report.xls");
-            Response.Charset = "";
-            Response.ContentType = "application/vnd.ms-excel";
-            using (StringWriter sw = new StringWriter())
-            {
-                HtmlTextWriter hw = new HtmlTextWriter(sw);
 
-                //To Export all pages
-                GridView1.AllowPaging = false;
-
-                GridView1.HeaderRow.BackColor = System.Drawing.Color.Transparent;
-                foreach (TableCell cell in GridView1.HeaderRow.Cells)
-                {
-                    cell.BackColor = GridView1.HeaderStyle.BackColor;
-                }
-                foreach (GridViewRow row in GridView1.Rows)
-                {
-                    row.BackColor = System.Drawing.Color.Transparent;
-
-                    foreach (TableCell cell in row.Cells)
-                    {
-                        if (row.RowIndex % 2 == 0)
-                        {
-                            cell.BackColor = GridView1.AlternatingRowStyle.BackColor;
-                        }
-                        else
-                        {
-                            cell.BackColor = GridView1.RowStyle.BackColor;
-                        }
-                        cell.CssClass = "textmode";
-                    }
-                }
-
-                GridView1.RenderControl(hw);
-
-                //style to format numbers to string
-                string style = @"<style> .textmode { } </style>";
-                Response.Write(style);
-                Response.Output.Write(sw.ToString());
-                Response.Flush();
-                Response.End();
-            }
-        }
-        public override void VerifyRenderingInServerForm(Control control)
-        {
-            /* Verifies that the control is rendered */
         }
 
        
