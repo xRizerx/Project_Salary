937e37128174b22d0af1c60ba85143c5f4df41ca
Jomjak Tangnopakoon <tangkwa_ba_mark@hotmail.com>
2015-11-18 12:17:57 +0700

    not yet

--- WEB_PERSONAL/WEB_PERSONAL/MasterPage.Master.designer.cs
+++ WEB_PERSONAL/WEB_PERSONAL/MasterPage.Master.designer.cs
@@ -40,6 +40,15 @@ namespace WEB_PERSONAL {
         protected global::System.Web.UI.WebControls.Label Label11;
         
         /// <summary>
+        /// Panel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel1;
+        
+        /// <summary>
         /// Label12X control.
         /// </summary>
         /// <remarks>
