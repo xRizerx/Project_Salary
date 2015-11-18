854a07986f45f4278ef070ab1a89b34672b8004b
Tanakorn Na Ranong <tanakorn_seed@hotmail.com>
2015-11-18 12:06:21 +0700

    35436

--- WEB_PERSONAL/WEB_PERSONAL/insignia_from.aspx.designer.cs
+++ WEB_PERSONAL/WEB_PERSONAL/insignia_from.aspx.designer.cs
@@ -103,13 +103,13 @@ namespace WEB_PERSONAL {
         protected global::System.Web.UI.WebControls.Label Label11;
         
         /// <summary>
-        /// TextBox1 control.
+        /// DropDownList9 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList9;
         
         /// <summary>
         /// Label12 control.
