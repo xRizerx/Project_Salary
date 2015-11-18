6f6e31a96ba1d0e43e11a61c4142743e0b9a6313
Jomjak Tangnopakoon <tangkwa_ba_mark@hotmail.com>
2015-11-18 14:50:30 +0700

    xdxd

--- WEB_PERSONAL/WEB_PERSONAL/Leave.aspx.designer.cs
+++ WEB_PERSONAL/WEB_PERSONAL/Leave.aspx.designer.cs
@@ -704,23 +704,5 @@ namespace WEB_PERSONAL {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView GridView1;
-        
-        /// <summary>
-        /// Label35 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label35;
-        
-        /// <summary>
-        /// LinkButton19 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton LinkButton19;
     }
 }
