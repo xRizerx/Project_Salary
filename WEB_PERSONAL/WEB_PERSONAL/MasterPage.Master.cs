7b201e1395df2a1fe34a64a5efd1d37b54eaca70
Jomjak Tangnopakoon <tangkwa_ba_mark@hotmail.com>
2015-12-01 12:33:43 +0700

    minor update login

--- WEB_PERSONAL/WEB_PERSONAL/MasterPage.Master.cs
+++ WEB_PERSONAL/WEB_PERSONAL/MasterPage.Master.cs
@@ -7,53 +7,43 @@ using System.Web.UI.WebControls;
 using System.Data.OracleClient;
 using WEB_PERSONAL.Class;
 
-namespace WEB_PERSONAL
-{
-    public partial class MasterPage : System.Web.UI.MasterPage
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            
+namespace WEB_PERSONAL {
+    public partial class MasterPage : System.Web.UI.MasterPage {
+        protected void Page_Load(object sender, EventArgs e) {
+
         }
 
-        protected void LinkButton4_Click(object sender, EventArgs e)
-        {
+        protected void LinkButton4_Click(object sender, EventArgs e) {
             /*Response.Redirect("Salary.aspx");*/
         }
 
-        protected void LinkButton5_Click(object sender, EventArgs e)
-        {
+        protected void LinkButton5_Click(object sender, EventArgs e) {
             Response.Redirect("Default.aspx");
         }
 
-        protected void LinkButton1_Click(object sender, EventArgs e)
-        {
+        protected void LinkButton1_Click(object sender, EventArgs e) {
             Response.Redirect("Leave.aspx");
         }
 
-        protected void LinkButton6_Click(object sender, EventArgs e)
-        {
+        protected void LinkButton6_Click(object sender, EventArgs e) {
             Response.Redirect("SEMINAR-GENERAL.aspx");
         }
 
-        protected void LinkButton2_Click(object sender, EventArgs e)
-        {
+        protected void LinkButton2_Click(object sender, EventArgs e) {
             Response.Redirect("insignia_citizen.aspx");
         }
 
-        protected void LinkButton9_Click(object sender, EventArgs e)
-        {
+        protected void LinkButton9_Click(object sender, EventArgs e) {
             Response.Redirect("Login.aspx");
         }
 
-        protected void form1_Load(object sender, EventArgs e)
-        {
+        protected void form1_Load(object sender, EventArgs e) {
             if (Session["login_id"] == null) {
                 Session["redirect_to"] = Request.Url.ToString();
                 Response.Redirect("Access.aspx");
                 return;
             } else {
-                
+
                 if ((DateTime.Now - (DateTime)Session["login_date_time"]).TotalSeconds > Int32.Parse(Session["login_total_second"].ToString())) {
                     Session["login_date_time"] = null;
                     Logout();
@@ -64,85 +54,70 @@ namespace WEB_PERSONAL
                 }
                 Person person = ((Person)Session["login_person"]);
 
-
                 /*Label7.Text = Session["login_id"].ToString();*/
                 //string name = Session["login_name"].ToString() + " " + Session["login_lastname"];
                 string name = person.NameAndLastname;
-                string systemRank = "(" + Session["login_system_status"].ToString() + ")";
+                //string systemRank = "(" + Session["login_system_status"].ToString() + ")";
                 LinkButton1.Text = name;
                 //Label7.Text = systemRank;
                 LinkButton10.Visible = true;
-                if(Session["login_system_status_id"].ToString() == "1")
-                {
-                    LinkButton1.ForeColor = System.Drawing.Color.FromArgb(204,41,57);
+                if (Session["login_system_status_id"].ToString() == "1") {
+                    LinkButton1.ForeColor = System.Drawing.Color.FromArgb(204, 41, 57);
                     //Label7.ForeColor = System.Drawing.Color.FromArgb(204, 41, 57);
-                } else
-                {
+                } else {
                     LinkButton1.ForeColor = System.Drawing.Color.FromArgb(0, 162, 232);
                     //Label7.ForeColor = System.Drawing.Color.FromArgb(0, 162, 232);
                 }
-                
+
             }
 
-            if(!IsPostBack) {
+            if (!IsPostBack) {
                 using (OracleConnection con = Util.OC()) {
-                    {
-                        string sql = "UPDATE TB_WEB SET COUNTER = COUNTER+1 WHERE ID = 1";
-                        using (OracleCommand command = new OracleCommand(sql, con)) {
-                            command.ExecuteNonQuery();
-                        }
+                    using (OracleCommand command = new OracleCommand("UPDATE TB_WEB SET COUNTER = COUNTER+1 WHERE ID = 1", con)) {
+                        command.ExecuteNonQuery();
                     }
-                    {
-                        string sql = "SELECT COUNTER FROM TB_WEB WHERE ID = 1";
-                        using (OracleCommand command = new OracleCommand(sql, con)) {
-                            using (OracleDataReader reader = command.ExecuteReader()) {
-                                if (reader.HasRows) {
-                                    reader.Read();
-                                    LabelCounter.Text = "จำนวนผู้เข้าชม : " + reader.GetInt32(0).ToString("#,###") + " ครั้ง";
-                                }
+                    using (OracleCommand command = new OracleCommand("SELECT COUNTER FROM TB_WEB WHERE ID = 1", con)) {
+                        using (OracleDataReader reader = command.ExecuteReader()) {
+                            if (reader.HasRows) {
+                                reader.Read();
+                                LabelCounter.Text = "จำนวนผู้เข้าชม : " + reader.GetInt32(0).ToString("#,###") + " ครั้ง";
                             }
-
                         }
                     }
                 }
             }
-            
+
         }
 
-        protected void LinkButton10_Click(object sender, EventArgs e)
-        {
+        protected void LinkButton10_Click(object sender, EventArgs e) {
             //log out
             Logout();
 
             Response.Redirect("Access.aspx");
         }
 
-        protected void LinkButton7_Click(object sender, EventArgs e)
-        {
+        protected void LinkButton7_Click(object sender, EventArgs e) {
             Response.Redirect("Salary.aspx");
         }
 
-        protected void LinkButton8_Click(object sender, EventArgs e)
-        {
+        protected void LinkButton8_Click(object sender, EventArgs e) {
             Response.Redirect("SalarybyID.aspx");
         }
 
-        protected void LinkButton3_Click(object sender, EventArgs e)
-        {
+        protected void LinkButton3_Click(object sender, EventArgs e) {
             Response.Redirect("Personnel-GENERAL.aspx");
         }
 
-        protected void LinkButton11_Click(object sender, EventArgs e)
-        {
+        protected void LinkButton11_Click(object sender, EventArgs e) {
             Response.Redirect("Study-IN.aspx");
         }
 
-        protected void LinkButton12_Click(object sender, EventArgs e)
-        {
+        protected void LinkButton12_Click(object sender, EventArgs e) {
             Response.Redirect("M-Admin.aspx");
         }
 
         private void Logout() {
+            Session.Remove("login_person");
             Session.Remove("login_id");
             Session.Remove("login_system_status");
             Session.Remove("login_name");
@@ -159,7 +134,7 @@ namespace WEB_PERSONAL
             Response.Redirect("Person-General.aspx");
         }
 
-       
+
 
         protected void LinkButton1_Click1(object sender, EventArgs e) {
             Response.Redirect("Profile.aspx");
